a92d6ce755fdf989ff832f37b3cf6b69686e9269
infzer0 <johnortizo@outlook.com>
2020-08-22 17:48:11 -0500

    Java Ejercicio: 345 Representar Caracteres Unicode sobre Variables Tipo char
    Representar Caracteres Unicode sobre Variables Tipo char con el lenguaje de programación Java.

---
+++ JavaEjercicios/src/main/java/ejercicio0000345/CaracteresUnicode.java
@@ -0,0 +1,23 @@
+package ejercicio0000345;
+
+/**
+ * Ejercicio 345: Representar caracteres Unicode en variables tipo char.
+ *
+ * @author John Ortiz Ordoñez
+ */
+public class CaracteresUnicode {
+
+    public static void main(String[] args) {
+        char unicode = '\u0021';
+        System.out.println(unicode);
+        
+        System.out.println();
+        
+        unicode = '\u0040';
+        System.out.println(unicode);
+        
+        System.out.println();
+        
+        System.out.println('\u00AE');
+    }
+}
