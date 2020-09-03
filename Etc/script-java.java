2b57c0b48f9617310e0e3d8810ee7f9cc121318d
infzer0 <johnortizo@outlook.com>
2020-09-03 06:45:06 -0500

    Java Ejercicio: 351 Ejecución de un Script Java a través de la Herramienta de Línea de Comandos JShell
    Ejecución de un Script Java a través de la Herramienta de Línea de Comandos JShell con el lenguaje de Programación Java.

---
+++ Etc/script-java.java
@@ -0,0 +1,12 @@
+System.out.println("Hola, JShell!")
+
+String texto = "Java facilita el desarrollo de software."
+
+System.out.println(texto)
+
+int a = 2
+int b = 3
+int suma = a + b
+System.out.println(suma)
+
+/exit
