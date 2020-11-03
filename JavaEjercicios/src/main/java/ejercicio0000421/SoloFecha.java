c5e309455b4732eae22e4dfabbbb608665e063c2
infzer0 <johnortizo@outlook.com>
2020-11-03 17:57:45 -0500

    Java Ejercicio: 421 Obtener la Fecha y Hora Actual con la Clase Date desde el Paquete java.util
    Obtener la Fecha y Hora Actual con la Clase Date desde el Paquete java.util con el lenguaje de programación Java.

---
+++ JavaEjercicios/src/main/java/ejercicio0000421/SoloFecha.java
@@ -0,0 +1,19 @@
+package ejercicio0000421;
+
+import java.util.Date;
+
+/**
+ * Ejercicio 421: Obtener información de la fecha y hora con la clase 
+ * java.util.Date.
+ *
+ * @author John Ortiz Ordoñez
+ */
+public class SoloFecha {
+
+    public static void main(String[] args) {
+        
+        Date fechaHoraActuales = new Date();
+        
+        System.out.println("Fecha y hora actual: " + fechaHoraActuales);
+    }
+}
