d1496df65408bdb762d7046a9c14349daaa32f94
infzer0 <johnortizo@outlook.com>
2020-11-21 12:33:02 -0500

    Java Ejercicio: 438 Declarar Literales Binarias sobre Variables de Tipo int
    Declarar Literales Binarias sobre Variables de Tipo int con el lenguaje de programación Java.

---
+++ JavaEjercicios/src/main/java/ejercicio0000438/LiteralesBinarias.java
@@ -0,0 +1,25 @@
+package ejercicio0000438;
+
+/**
+ * Ejercicio 438: Declarar literales binarias. 
+ *
+ * @author John Ortiz Ordoñez
+ */
+public class LiteralesBinarias {
+
+    public static void main(String[] args) {
+        
+        int doce = 0b1100;
+        System.out.println(doce);   // 12
+        
+        System.out.println();
+        
+        int quince = 0B1111;
+        System.out.println(quince); // 15
+        
+        System.out.println();
+        
+        int binario = 0b11100110011;
+        System.out.println(binario);
+    }
+}
