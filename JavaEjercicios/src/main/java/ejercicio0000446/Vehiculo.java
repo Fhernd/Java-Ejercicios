4487dd471ddba61ccae84eacfeef55dfdb06b21f
infzer0 <johnortizo@outlook.com>
2020-12-04 07:32:37 -0500

    Java Ejercicio: 446 Crear una Interfaz que Describa la Funcionliadad Común entre Diferentes Tipos de Objetos
    Crear una Interfaz que Describa la Funcionliadad Común entre Diferentes Tipos de Objetos con el lenguaje de programación Java.

---
+++ JavaEjercicios/src/main/java/ejercicio0000446/Vehiculo.java
@@ -0,0 +1,13 @@
+package ejercicio0000446;
+
+/**
+ * Ejercicio 446: Crear una interfaz que describa la funcionalidad común para 
+ * todo tipo de vehículo.
+ * 
+ * @author John Ortiz Ordoñez.
+ */
+public interface Vehiculo {
+    void acelarar();
+    void frenar();
+    void estacionarse();
+}
