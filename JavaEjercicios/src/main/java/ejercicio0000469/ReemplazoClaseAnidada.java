8cd2839595f3d2e392c42b66029b09cc8f0b99d6
John Ortiz Ordoñez <johnortizo@outlook.com>
2021-01-22 09:55:24 -0500

    Java Ejercicio: 469 Reemplazar el Uso de Clases Anidadas por Expresiones Lambda en una GUI
    Reemplazar el Uso de Clases Anidadas por Expresiones Lambda en una GUI con el lenguaje de programación Java.

---
+++ JavaEjercicios/src/main/java/ejercicio0000469/ReemplazoClaseAnidada.java
@@ -0,0 +1,14 @@
+package ejercicio0000469;
+
+/**
+ * Ejercicio 469: Reemplazar el uso de clases anidadas por una expresión lambda 
+ * para eventos de una GUI.
+ * 
+ * @author John Ortiz Ordoñez
+ */
+public class ReemplazoClaseAnidada {
+
+    public static void main(String[] args) {
+        
+    }
+}
