ac0177d7d563fbc0a15fec37f2a620b2847a4bb3
infzer0 <johnortizo@outlook.com>
2021-01-30 09:27:20 -0500

    Java Ejercicio: 480 2/2 Usar una Clase para Crear un Iterador de Objetos Contenidos en una Colección
    Usar una Clase para Crear un Iterador de Objetos Contenidos en una Colección con el lenguaje de programación Java.

--- JavaEjercicios/src/main/java/ejercicio0000480/CreacionIterable.java
+++ JavaEjercicios/src/main/java/ejercicio0000480/CreacionIterable.java
@@ -1,6 +1,8 @@
 package ejercicio0000480;
 
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
 
 /**
  * Ejercicio 480: Usar una clase para hacer de un conjunto de objetos iterable.
@@ -10,17 +12,41 @@ import java.util.Iterator;
 public class CreacionIterable {
 
     public static void main(String[] args) {
-
+        Almacen almacenComputadores = new Almacen();
+        
+        almacenComputadores.agregarComputador(new Computador(1001, "Clone", "Intel Core i7", 128, 8000));
+        almacenComputadores.agregarComputador(new Computador(1002, "Apple", "Intel Core i5", 16, 720));
+        almacenComputadores.agregarComputador(new Computador(1003, "MSi", "Intel Core i7", 32, 1024));
+        almacenComputadores.agregarComputador(new Computador(1004, "Asus", "Intel Core i7", 16, 500));
+        
+        // Iteración de un objeto `Almacen` con un ciclo for mejorado:
+        for (Computador c : almacenComputadores) {
+            System.out.println(c);
+        }
+        
+        System.out.println();
+        
+        // Iteración de un objeto `Almacen` con una expresión lambda:
+        almacenComputadores.forEach(c -> System.out.println(c));
     }
 }
 
 class Almacen implements Iterable<Computador> {
+    
+    private Map<Integer, Computador> computadores;
+
+    public Almacen() {
+        computadores = new HashMap<>();
+    }
+    
+    public void agregarComputador(Computador computador) {
+        computadores.put(computador.getId(), computador);
+    }
 
     @Override
     public Iterator<Computador> iterator() {
-        
+        return computadores.values().iterator();
     }
-    
 }
 
 class Computador {
@@ -87,5 +113,10 @@ class Computador {
 
     public void setSsd(int ssd) {
         this.ssd = ssd;
+    }
+
+    @Override
+    public String toString() {
+        return "Computador{" + "id=" + id + ", marca=" + marca + ", cpu=" + cpu + ", ram=" + ram + ", ssd=" + ssd + '}';
     }
 }
