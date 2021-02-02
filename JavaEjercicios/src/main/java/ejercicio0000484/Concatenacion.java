cabb06661b648d45d2f37c60d328ff717ef8df1f
infzer0 <johnortizo@outlook.com>
2021-02-02 14:34:20 -0500

    Java Ejercicio: 484 Usar la Clase StringBuilder para Concatenar Cadenas de Caracteres (Texto)
    Usar la Clase StringBuilder para Concatenar Cadenas de Caracteres (Texto) con el lenguaje de programación Java.

---
+++ JavaEjercicios/src/main/java/ejercicio0000484/Concatenacion.java
@@ -0,0 +1,31 @@
+package ejercicio0000484;
+
+/**
+ * Ejercicio 484: Usar la clase StringBuilder para concatenar cadenas de 
+ * caracteres (texto).
+ *
+ * @author John Ortiz Ordoñez
+ */
+public class Concatenacion {
+
+    public static void main(String[] args) {
+        
+        StringBuilder nombreCompleto = new StringBuilder("Irlesa ");
+        nombreCompleto.append("Indira ");
+        nombreCompleto.append("Ocampo ");
+        nombreCompleto.append("Romero");
+        
+        String resultado = nombreCompleto.toString();
+        
+        System.out.println(resultado);
+        
+        System.out.println();
+        
+        // vs String:
+        String fullName = "Irlesa ";    // 1. "Irlesa"
+        fullName += "Indira "; // 2. "Irlesa Indira "
+        fullName += "Ocampo "; // 3. "Irlesa Indira Ocampo "
+        fullName += "Romero";  // 4. "Irlesa Indira Ocampo Romero"
+        System.out.println(fullName);
+    }
+}
