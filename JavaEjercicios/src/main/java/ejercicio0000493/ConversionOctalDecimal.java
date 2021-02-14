537329278a19ed8b18261cdb4dfe43f69ea4ab84
infzer0 <johnortizo@outlook.com>
2021-02-14 12:38:09 -0500

    Java Ejercicio: 493 Definir una Función para Convertir un Número Octal en un Número Decimal
    Definir una Función para Convertir un Número Octal en un Número Decimal con el lenguaje de programación Java.

---
+++ JavaEjercicios/src/main/java/ejercicio0000493/ConversionOctalDecimal.java
@@ -0,0 +1,32 @@
+package ejercicio0000493;
+
+/**
+ * Ejercicio 493: Definir una función para convertir un número octal en un 
+ * número decimal.
+ *
+ * @author John Ortiz Ordoñez
+ */
+public class ConversionOctalDecimal {
+
+    public static void main(String[] args) {
+        
+        long octal = 12;
+        
+        System.out.println(convertirOctalDecimal(octal));   // 8
+    }
+    
+    public static long convertirOctalDecimal(long octal) {
+        int i = 0;
+        long decimal = 0;
+        
+        while (octal != 0) {
+            decimal = (long) (decimal + (octal % 10) * Math.pow(8, i));
+            
+            octal /= 10;
+            
+            ++i;
+        }
+        
+        return decimal;
+    }
+}
