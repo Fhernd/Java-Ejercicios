7ec33eb159b952f0a18b3dcdf2e4035a0831110b
infzer0 <johnortizo@outlook.com>
2021-03-05 17:32:35 -0500

    Java Ejercicio: 518 Convertir una Cantidad de Segundos en Horas, Minutos, y Segundos
    Convertir una Cantidad de Segundos en Horas, Minutos, y Segundos con el lenguaje de programación Java.

---
+++ JavaEjercicios/src/main/java/ejercicio0000518/ConversionSegundos.java
@@ -0,0 +1,30 @@
+package ejercicio0000518;
+
+/**
+ * Ejercicio 518: Convertir una cantidad de segundos en horas, minutos, y segundos.
+ *
+ * @author John Ortiz Ordoñez
+ */
+public class ConversionSegundos {
+
+    public static void main(String[] args) {
+        
+        int segundos = 83237;
+        int[] resultado = convertirTiempo(segundos);
+        
+        System.out.println("Horas: " + resultado[0]);
+        System.out.println("Minutos: " + resultado[1]);
+        System.out.println("Segundos: " + resultado[2]);
+    }
+    
+    public static int[] convertirTiempo(int segundos) {
+        int horas = segundos / 3600;
+        segundos %= 3600;
+        int minutos = segundos / 60;
+        segundos %= 60;
+        
+        int[] tiempo = {horas, minutos, segundos};
+        
+        return tiempo;
+    }
+}
