f6f0839df1831b62ddf40e52d5fa221c8138ef22
infzer0 <johnortizo@outlook.com>
2021-03-19 14:50:06 -0500

    Java Ejercicio: 532 Definir una Función para Remover el Primer Carácter de Cada Palabra de una Frase
    Definir una Función para Remover el Primer Carácter de Cada Palabra de una Frase con el lenguaje de programación Java.

---
+++ JavaEjercicios/src/main/java/ejercicio0000532/RemocionPrimerCaracter.java
@@ -0,0 +1,38 @@
+package ejercicio0000532;
+
+/**
+ * Ejercicio 532: Definir una función para remover el primer carácter de cada palabra de una frase.
+ *
+ * @author John Ortiz Ordoñez
+ */
+public class RemocionPrimerCaracter {
+
+    public static void main(String[] args) {
+        
+        String frase = "Java es un lenguaje de programación y plataforma de desarrollo";
+        
+        String resultado = removerPrimerCaracterPorPalabra(frase);
+        
+        
+        System.out.println(frase);
+        System.out.println(resultado);
+    }
+    
+    public static String removerPrimerCaracterPorPalabra(String frase) {
+        String[] palabras = frase.split(" ");
+        
+        String resultado = "";
+        
+        for (String p : palabras) {
+            if (p.length() > 1) {
+                resultado += p.substring(1, p.length());
+            } else {
+                resultado += p;
+            }
+            
+            resultado += " ";
+        }
+        
+        return resultado;
+    }
+}
