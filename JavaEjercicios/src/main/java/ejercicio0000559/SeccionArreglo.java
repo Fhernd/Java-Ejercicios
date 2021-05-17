f2de2ae7cc299eecfd32ec75c8cc2f91ed668370
infzer0 <johnortizo@outlook.com>
2021-05-17 16:39:21 -0500

    Java Ejercicio: 559 Extraer los Valores que se Hallen Antes de un Valor Dado en un Arreglo
    Extraer los Valores que se Hallen Antes de un Valor Dado en un Arreglo con el lenguaje de programación Java.

---
+++ JavaEjercicios/src/main/java/ejercicio0000559/SeccionArreglo.java
@@ -0,0 +1,36 @@
+package ejercicio0000559;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+/**
+ * Ejercicio 559: Extraer todos los datos de un arreglo que están antes de un dato dado.
+ *
+ * @author John Ortiz Ordoñez.
+ */
+public class SeccionArreglo {
+
+    public static void main(String[] args) {
+        
+        int[] numeros = {11, 7, 5, 19, 29, 31, 2, 43, 13, 17};
+        
+        System.out.println(Arrays.toString(numeros));
+        
+        System.out.println();
+        
+        int numero = 11;
+        
+        int indice = Arrays.stream(numeros).boxed().collect(Collectors.toList()).indexOf(numero);
+        
+        if (numero != -1) {
+            
+            int[] resultado = Arrays.copyOfRange(numeros, 0, indice);
+            
+            System.out.println("Resultado:");
+            System.out.println(Arrays.toString(resultado));
+            
+        } else {
+            System.out.println("MENSAJE: El valor buscado no se encuentra en el arreglo.");
+        }
+    }
+}
