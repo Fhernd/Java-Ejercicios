990a660d584b40f33bfd81b6e430dd02a5f92d9d
infzer0 <johnortizo@outlook.com>
2022-03-04 16:17:20 -0500

    Java Ejercicio: 602 Convertir a Minúsculas un Texto que se Halla en Mayúsculas con toLowerCase()
    Convertir a Minúsculas un Texto que se Halla en Mayúsculas con toLowerCase() con el lenguaje de programación Java.

---
+++ JavaEjercicios/src/main/java/ejercicio0000602/ConversionMinusculas.java
@@ -0,0 +1,19 @@
+package ejercicio0000602;
+
+/**
+ * Ejercicio 602: Convertir un texto en mayúsculas en minúsculas con toLowerCase().
+ *
+ * @author John Ortiz Ordoñez.
+ */
+public class ConversionMinusculas {
+
+    public static void main(String[] args) {
+        String frase = "JAVA ES UN LENGUAJE DE PROGRAMACIÓN";
+        System.out.println(frase);
+        
+        System.out.println();
+        
+        String resultado = frase.toLowerCase();
+        System.out.println(resultado);
+    }
+}
