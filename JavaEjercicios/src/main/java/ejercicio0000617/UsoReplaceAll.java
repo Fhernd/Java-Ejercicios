5d2040aa1794de5dae2379ea6acce32472b22ec4
infzer0 <johnortizo@outlook.com>
2022-04-28 17:21:58 -0500

    Java Ejercicio: 617 Usar la Función replaceAll() para Sustituir Todas las Coincidencias de un Cáracter
    Usar la Función replaceAll() para Sustituir Todas las Coincidencias de un Cáracter con el lenguaje de programación Java.

---
+++ JavaEjercicios/src/main/java/ejercicio0000617/UsoReplaceAll.java
@@ -0,0 +1,13 @@
+package ejercicio0000617;
+
+/**
+ * Ejercicio 617: Usar la función replaceAll() para reemplazar todas las coincidencias de un carácter por otro.
+ *
+ * @author: John Ortiz Ordoñez
+ */
+public class UsoReplaceAll {
+    public static void main(String[] args) {
+        System.out.println("COlOmbia".replaceAll("O", "o")); // Colombia
+        System.out.println("C0l0mbia".replaceAll("0", "o")); // Colombia
+    }
+}
